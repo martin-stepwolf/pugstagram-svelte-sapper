ebcf22578df14e344b79e759b25b00826b837ac6
martin-stepwolf <15151130@aguascalientes.tecnm.mx>
2020-11-21 11:12:09 -0600

    main structure(styles)

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,11 +1,16 @@
-<script>
-    let year = new Date().getFullYear();
-</script>
-  
+<style>
+    .Footer {
+      font-size: 11px;
+      letter-spacing: 1px;
+      font-weight: normal;
+      color: #c7c7c7;
+    }
+</style>
+
 <div class="Footer">
     <div class="Footer-container">
-        <small class="Footer-copy">
-        © { year } Pugstagram from platzi
-        </small>
+        <div class="Footer-copy">
+        © 2020 Pugstagram from platzi
+        </div>
     </div>
 </div>
